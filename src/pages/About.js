3d0d5cf088e916e6c19022b7961d024eb8dd6664
axie22 <alexxie9667@gmail.com>
2024-06-06 10:39:31 -0400

    updated about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profilePic from "../assets/profilePictureIBM.jpg"; 
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img className="profile_image" src={profilePic} alt="Profile Pic"></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Alexander Xie</div>
+              <div className="brief_description">
+                <p>I'm currently a sophomore at NYU studying computer science and data science</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
